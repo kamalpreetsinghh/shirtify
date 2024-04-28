456b8fbf978586de302e78bd77832e85e9572563
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-28 19:47:08 +0530

    Error Fixes in Camera Rig

--- components/canvas/CameraRig.tsx
+++ components/canvas/CameraRig.tsx
@@ -3,13 +3,18 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
+import { Group } from "three";
+
+interface GroupRef {
+  current: Group | null;
+}
 
 const CameraRig = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const group = useRef(null);
+  const group: GroupRef = useRef<Group>(null);
 
   useFrame((state, delta) => {
     const isWeb = window.innerWidth <= 1260;
@@ -25,12 +30,14 @@ const CameraRig = ({
     easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
     // set the model rotation
-    easing.dampE(
-      group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x / 5, 0],
-      0.25,
-      delta
-    );
+    if (group.current && group.current.rotation) {
+      easing.dampE(
+        group.current?.rotation,
+        [state.pointer.y / 10, -state.pointer.x / 5, 0],
+        0.25,
+        delta
+      );
+    }
   });
 
   return <group ref={group}>{children}</group>;
